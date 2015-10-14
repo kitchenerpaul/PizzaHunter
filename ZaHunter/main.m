21968d9ddddcc6e7bbaa8bd2d5c03c0c232a5bdd
Paul Kitchener <p.a.kitchener@gmail.com>
2015-10-14 15:56:51 -0500

    Initial Commit

---
+++ ZaHunter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZaHunter
+//
+//  Created by Paul Kitchener on 10/14/15.
+//  Copyright © 2015 Paul Kitchener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
